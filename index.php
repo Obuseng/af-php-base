60199c34361c5c46b0f1dbcb1787a79b3173b9a3
Obuseng <wdg.groups@gmail.com>
2013-05-31 14:12:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php echo "Hello world!"; ?>
+<?php 
+
+class addUser{
+
+public $randomtxt;
+
+public function __construct(){
+  $this->randomtxt = "I am a random text used for testing purposes only";
+  
+}
+
+
+//echo "Hello world!";
+
+
+}
+
+$loom = new addUser();
+
+?>
